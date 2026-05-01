1adea17b44926caff9be6d3f9ee5a4df4c6ed644
VladT0911 <vlad.tataru@student.upt.ro>
2026-05-01 12:09:17 +0300

    Add dashboard layout and HTTP client integration

--- erp-backend/frontend/src/app/dialog-box/dialog-box.ts
+++ erp-backend/frontend/src/app/dialog-box/dialog-box.ts
@@ -3,12 +3,37 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import {MatDialogModule} from '@angular/material/dialog';
+import {MatDialogModule, MatDialog} from '@angular/material/dialog';
+import {ConfirmDialog} from '../confirm-dialog/confirm-dialog';
+
 
 @Component({
   selector: 'app-dialog-box',
+  standalone: true,
   imports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatDialogModule],
   templateUrl: './dialog-box.html',
   styleUrl: './dialog-box.css',
 })
-export class DialogBox {}
+export class DialogBox {
+  constructor(private dialog: MatDialog){}
+  onCreateOrder(){
+   const dialogRef=this.dialog.open(ConfirmDialog,{
+      width:'300px',
+      data:{
+        title:"Confirm Order",
+        message:' Are you sure you want to create this order?'}
+        });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if(result===true) {
+        this.createOrder();
+          }
+        });
+      }
+
+
+        createOrder(){
+          console.log('Order created!');
+         }
+    }
+
