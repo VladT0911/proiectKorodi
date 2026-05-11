2c2e8f62714ea4f4c2823d9230e136fb09820e66
Vlad <vladmihai0911@gmail.com>
2026-05-11 23:39:59 +0300

    Update confirm dialog layout and functionality; change server port

--- erp-backend/frontend/src/app/confirm-dialog/confirm-dialog.ts
+++ erp-backend/frontend/src/app/confirm-dialog/confirm-dialog.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
   templateUrl: './confirm-dialog.html',
-  styleUrl: './confirm-dialog.css',
+  styleUrl: './confirm-dialog.css'
 })
-export class ConfirmDialog {}
+export class ConfirmDialog {
+  private dialogRef = inject(MatDialogRef<ConfirmDialog>);
+
+  confirm(): void {
+    this.dialogRef.close(true); // 👈 sends true back to DialogBox
+  }
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+}
