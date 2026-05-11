8e30a8fb46ced572498d254b4d3dc4d1793efab3
Vlad <vladmihai0911@gmail.com>
2026-05-11 23:39:50 +0300

    Add routing for dashboard component and clean up imports in app component

--- erp-backend/frontend/src/app/app.ts
+++ erp-backend/frontend/src/app/app.ts
@@ -1,25 +1,16 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { DialogBox } from './dialog-box/dialog-box'
-import { MatDialog } from '@angular/material/dialog';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {Dashboard} from "./dashboard/dashboard";
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports: [RouterOutlet,MatButtonModule,MatCardModule,MatIconModule,Dashboard],
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
-  readonly dialog=inject(MatDialog);
-  openDialog():void{
-    this.dialog.open(DialogBox)}
-
-
 }
