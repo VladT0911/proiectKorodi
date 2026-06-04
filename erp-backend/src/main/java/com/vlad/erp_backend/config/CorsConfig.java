0322160041a9c967b669a08f49be0dd34442c34c
Vlad <vladmihai0911@gmail.com>
2026-06-05 00:05:27 +0300

    Comment out security configuration and package declarations in CorsConfig and SecurityConfig

--- erp-backend/src/main/java/com/vlad/erp_backend/config/CorsConfig.java
+++ erp-backend/src/main/java/com/vlad/erp_backend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.vlad.erp_backend.config;
+/*package com.vlad.erp_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,3 +24,4 @@ public class CorsConfig {
         return source;
     }
 }
+*/
